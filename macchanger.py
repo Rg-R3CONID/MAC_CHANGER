3b2251514de2e2a82defb024b143660de28c2dbc
Rg-R3CONID <101502851+Rg-R3CONID@users.noreply.github.com>
2022-03-17 01:59:48 -0400

    march 17 2022
    Its a programme or script to change mac address.its a simple programme.

---
+++ macchanger.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+import subprocess
+import optparse
+
+def get_arguments():
+    parser = optparse.OptionParser()
+    parser.add_option("-i", "--interface", dest="interface", help="interface to change its MAC ADDRESS")
+    parser.add_option("-m", "--mac", dest="new_mac", help="New Mac Address")
+    (options, arguments) = parser.parse_args()
+    if not options.interface:
+        parser.error("[-] please specify an interface, use --help for more info.")
+    elif not options.new_mac:
+        parser.error("[-] please specify an new mac, use --help for more info.")
+    return options
+
+
+def change_mac(interface, new_mac):
+    print("[+] changing MAC address for" + interface + "to" + new_mac)
+    subprocess.call(["ifconfig", interface, "down"])
+    subprocess.call(["ifconfig", interface, "hw", "ether" , new_mac])
+    subprocess.call(["ifconfig", interface, "up"])
+
+options = get_arguments()
+change_mac(options.interface, options.new_mac)
